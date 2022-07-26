d9b13f7cbe4ebaf236c291ef3beeb1ed7e3fbf83
Matias Gonzalez <matias.gonzaleztt@gmail.com>
2022-07-25 23:35:49 -0300

    Agregamos array

--- javascrip/scrip.js
+++ javascrip/scrip.js
@@ -1,10 +1,23 @@
 alert("Bienvenidos al buen comer, el precio es por persona.")
 
 let comida = parseInt (prompt("Ingrese el numero segun corresponda de lo que va a comer 1- Pizza: $400 2- Hamburguesa: $500 3- Pasta: $300:  "))
-let personas = parseInt (prompt("Ingrese el numero de personas que van a comer: "))
+let personas = parseInt (prompt("Ingrese el numero de personas que van a comer, recuerde que solo se permiten 4 personas: "))
 
 console.log ("Comida seleccionada: " + comida)
 console.log ("Personas que van a comer: " + personas)
+
+const listaclientes = [];
+let cantidad = personas;
+
+do {
+    let entrada = prompt("Ingresar nombre: ");
+    listaclientes.push(entrada);
+    console.log(listaclientes.legth);
+} while (listaclientes.length != cantidad)
+    console.log (listaclientes)
+;
+alert("Los nombres de los clientes son: " + listaclientes)
+
 let precio = 0;
 
 if(comida == 1 ){
@@ -23,11 +36,12 @@ if(comida == 1 ){
 let preciototal = precio * personas
 console.log ("El precio total sin descuento es:" + preciototal)
 
-if (personas > 3){
-    let total = preciototal * 0.5
+if (personas > 2){
+    let total = preciototal * 0.2
     alert("El total a pagar es: " + total)
 } else {
     alert("El total a pagar es: " + preciototal)
 }
+
 
 alert("Ahora solo falta reservar tu mesa. Muchas gracias")
